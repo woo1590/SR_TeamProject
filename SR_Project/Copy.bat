6391a81439df60940d614b55c3109ef9978f22d5
woo <leewooseok1114@gmail.com>
2025-06-24 15:15:35 +0900

    copy bat 수정

--- SR_Project/Copy.bat
+++ SR_Project/Copy.bat
@@ -2,15 +2,15 @@
 cd /d %~dp0
 
 
-xcopy	/y	.\Engine\Header\*.*	.\Reference\Header
+xcopy	/y	.\Engine\Header\*.*	.\Reference\Header\
 
-xcopy	/y	.\Engine\Bin\Debug\Engine.dll	.\Client\Bin\Debug
-xcopy	/y	.\Engine\Bin\Debug\Engine.lib	.\Client\Bin\Debug
+xcopy	/y	.\Engine\Bin\Debug\Engine.dll	.\Client\Bin\Debug\
+xcopy	/y	.\Engine\Bin\Debug\Engine.lib	.\Client\Bin\Debug\
 
-xcopy	/y	.\Engine\Bin\Release\Engine.dll	.\Client\Bin\Release
-xcopy	/y	.\Engine\Bin\Release\Engine.lib	.\Client\Bin\Release
+xcopy	/y	.\Engine\Bin\Release\Engine.dll	.\Client\Bin\Release\
+xcopy	/y	.\Engine\Bin\Release\Engine.lib	.\Client\Bin\Release\
 
-xcopy	/y	.\Engine\Bin\Debug\Engine.lib	.\Reference\Library\Debug
-xcopy	/y	.\Engine\Bin\Release\Engine.lib	.\Reference\Library\Release
+xcopy	/y	.\Engine\Bin\Debug\Engine.lib	.\Reference\Library\Debug\
+xcopy	/y	.\Engine\Bin\Release\Engine.lib	.\Reference\Library\Release\
 
 pause
