2c5fd2675c701a0340d7207a2a8fcd64267f31d9
조혜연 <adencho1204@naver.com>
2025-07-04 14:33:14 +0900

    #123 크리퍼 걷는 모션 + 뼈 + 메쉬

--- SR_Project/Client/Code/TestScene.cpp
+++ SR_Project/Client/Code/TestScene.cpp
@@ -33,7 +33,7 @@
 #include "UILoader.h"
 #include "DirectionLight.h"
 #include "Skeleton.h"
-
+#include "Creeper.h"
 //component
 #include "TransformComponent.h"
 #include "CameraComponent.h"
@@ -96,6 +96,7 @@ void TestScene::Load()
 	ObjectMgr->AddObject(ObjectType::SkyBox, SkyBox::Create(ObjectMgr, ObjectType::SkyBox));
 	ObjectMgr->AddObject(ObjectType::Monster, Zombie::Create(ObjectMgr, ObjectType::Monster));
 	ObjectMgr->AddObject(ObjectType::Monster, Skeleton::Create(ObjectMgr, ObjectType::Monster));
+	ObjectMgr->AddObject(ObjectType::Monster, Creeper::Create(ObjectMgr, ObjectType::Monster));
 	
 
 	/*------------------Load UI------------------------*/
