112d58e32db128ed369c5a8375af07587eed0c5e
woo <leewooseok1114@gmail.com>
2025-07-13 14:43:12 +0900

    TestTerrain, Plain 지형 추가
    LoadChunk 부분에
    
    평지 테스트 필요하면 Plain넣고
    지형 보고 싶으면 TestTerrain 넣으셈

--- SR_Project/Client/Code/TestScene.cpp
+++ SR_Project/Client/Code/TestScene.cpp
@@ -82,7 +82,7 @@ void TestScene::Load()
 	BlockMgr		= BlockManager::Create(this);
 	ChunkMgr		= ChunkManager::Create(this);
 
-	BlockMgr->LoadChunk("testScene");
+	BlockMgr->LoadChunk("TestTerrain");
 
 	Grid->InsertBlock();
 	uiMgr           = UIManager::Create(this);
@@ -108,21 +108,21 @@ void TestScene::Load()
 	
 	/*------------------------------------------------*/
 	ObjectMgr->AddObject(ObjectType::SkyBox, SkyBox::Create(ObjectMgr, ObjectType::SkyBox));
-	//ObjectMgr->AddObject(ObjectType::Monster, RedGolem::Create(ObjectMgr, ObjectType::Monster));
+	ObjectMgr->AddObject(ObjectType::Monster, RedGolem::Create(ObjectMgr, ObjectType::Monster));
 
-	auto trigger = SpawnTriggerBox::Create(ObjectMgr, ObjectType::Neutral);
-	ObjectMgr->AddObject(ObjectType::Neutral, trigger);
+	//auto trigger = SpawnTriggerBox::Create(ObjectMgr, ObjectType::Neutral);
+	//ObjectMgr->AddObject(ObjectType::Neutral, trigger);
 
-	trigger->AddSpawner(SpawnType::Zombie,   _vec3(10.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
-	trigger->AddSpawner(SpawnType::Zombie,   _vec3(70.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
-	trigger->AddSpawner(SpawnType::Zombie,   _vec3(40.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
-	trigger->AddSpawner(SpawnType::Zombie,   _vec3(10.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
-	trigger->AddSpawner(SpawnType::Skeleton, _vec3(20.f, 10.f, 20.f), _vec3(0.f, 0.f, 0.f));
-	trigger->AddSpawner(SpawnType::Skeleton, _vec3(80.f, 10.f, 80.f), _vec3(0.f, 0.f, 0.f));
+	//trigger->AddSpawner(SpawnType::Zombie,   _vec3(10.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
+	//trigger->AddSpawner(SpawnType::Zombie,   _vec3(70.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
+	//trigger->AddSpawner(SpawnType::Zombie,   _vec3(40.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
+	//trigger->AddSpawner(SpawnType::Zombie,   _vec3(10.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
+	//trigger->AddSpawner(SpawnType::Skeleton, _vec3(20.f, 10.f, 20.f), _vec3(0.f, 0.f, 0.f));
+	//trigger->AddSpawner(SpawnType::Skeleton, _vec3(80.f, 10.f, 80.f), _vec3(0.f, 0.f, 0.f));
 	//trigger->AddSpawner(SpawnType::Creeper,  _vec3(30.f, 10.f, 30.f), _vec3(0.f, 0.f, 0.f));
 	//trigger->AddSpawner(SpawnType::Creeper,  _vec3(30.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
 	//trigger->AddSpawner(SpawnType::Creeper,  _vec3(30.f, 10.f, 50.f), _vec3(0.f, 0.f, 0.f));
-	trigger->AddSpawner(SpawnType::RedGolem, _vec3(5.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
+	//trigger->AddSpawner(SpawnType::RedGolem, _vec3(5.f, 10.f, 10.f), _vec3(0.f, 0.f, 0.f));
 
 	UILoader loader;
 	loader.LoadUI(ObjectMgr);
