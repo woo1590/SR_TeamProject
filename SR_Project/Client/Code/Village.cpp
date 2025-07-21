178739e08d29ce02cc41429d28c69a6abd6cf352
mmsphone <155417088+mmsphone@users.noreply.github.com>
2025-07-21 19:03:28 +0900

    #294 플레이어 소환높이 조정

--- SR_Project/Client/Code/Village.cpp
+++ SR_Project/Client/Code/Village.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "Village.h"
 #include "EngineCore.h"
 #include "LoadingScene.h"
@@ -165,7 +165,7 @@ void Village::Load()
 
 		if (game->IsSceneClear(LOADID::Village))
 		{
-			player->GetComponent<TransformComponent>()->SetPosition(160.f, 20.f, 200.f);
+			player->GetComponent<TransformComponent>()->SetPosition(160.f, 100.f, 200.f);
 		}
 		else
 		{
