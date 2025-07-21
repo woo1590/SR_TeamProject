095497c55b866bccb237794fb110385ece0ad945
조혜연 <adencho1204@naver.com>
2025-07-21 17:38:15 +0900

    #293 셜커 제작 완료

--- SR_Project/Client/Code/Shulker.cpp
+++ SR_Project/Client/Code/Shulker.cpp
@@ -3,6 +3,20 @@
 #include "TransformComponent.h"
 #include "CollisionComponent.h"
 #include "InfoComponent.h"
+#include "AIController.h"
+#include "IsAlive.h"
+#include "Die.h"
+#include "BehaviorTree.h"
+#include "Attack.h"
+#include "IsTargetInAttackRange.h"
+#include "BlackBoard.h"
+#include "ObjectManager.h"
+#include "SequenceNode.h"
+#include "SelectorNode.h"
+#include "ShulkerBullet.h"
+#include "PhysicsComponent.h"
+#include "EngineCore.h"
+#include "SoundManager.h"
 
 Shulker::Shulker(ObjectManager* owner, ObjectType objType)
 	:Monster(owner, objType)
@@ -35,7 +49,7 @@ HRESULT Shulker::Ready_Object(ObjectManager* owner, ObjectType objType)
 
     //Init Collision
     auto collision = GetComponent<CollisionComponent>();
-    collision->SetSize(_vec3(3.f, 3.f, 3.f));
+    collision->SetSize(_vec3(3.f, 3.5f, 3.f));
 
     //Create BT
     InitTree();
@@ -43,6 +57,7 @@ HRESULT Shulker::Ready_Object(ObjectManager* owner, ObjectType objType)
     //Animation
     InitAnimation();
 
+    InitBullet();
     GetComponent<InfoComponent<EnemyInfo>>()->SetInfo({ 1,100,100, 0, 0, 15, 0, 6 });
     
     return S_OK;
@@ -51,6 +66,7 @@ HRESULT Shulker::Ready_Object(ObjectManager* owner, ObjectType objType)
 void Shulker::Update(_float dt)
 {
     Monster::Update(dt);
+    PlayAnimation(dt);
 }
 
 void Shulker::Late_Update(_float dt)
@@ -60,10 +76,46 @@ void Shulker::Late_Update(_float dt)
 
 void Shulker::Attack(Object* target)
 {
+    if (State != MonsterState::Attack)
+    {
+        State = MonsterState::Attack;
+        AttackAnim.DelayTime = 0.f;
+        AttackAnim.ElapsedTime = 0;
+
+        AttackAnim.IsRunning = true;
+        AttackAnim.IsEnd = false;
+        *IsAttack = true;
+    }
 }
 
 void Shulker::Die()
 {
+    if (State != MonsterState::Die)
+    {
+        State = MonsterState::Die;
+
+        DieAnim.IsRunning = true;
+        DieAnim.IsEnd = false;
+
+        auto collision = GetComponent<CollisionComponent>();
+        collision->SetSize(_vec3(0.1f, 0.1f, 0.1f));
+
+        auto physics = GetComponent<PhysicsComponent>();
+        physics->SetGround(false);
+
+        EngineCore::GetInstance()->GetSoundManager()->PlaySFX("DeathShulker");
+    }
+}
+
+void Shulker::InitBullet()
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        auto bullet = ShulkerBullet::Create(owner, ObjectType::Projectile);
+        bullet->SetActive(false);
+        bullets.push_back(bullet);
+        owner->AddObject(ObjectType::Projectile, bullet);
+    }
 }
 
 void Shulker::InitTransform(ObjectType objType)
@@ -71,15 +123,16 @@ void Shulker::InitTransform(ObjectType objType)
     auto transform = AddComponent<TransformComponent>();
     Bones["Body"]->GetComponent<TransformComponent>()->SetParent(transform);
 
-    transform->SetRotate(_vec3(0.f, D3DXToRadian(90.f), 0.f));
-
     SetMaterial("ShulkerFace_Mtrl", "Body", RENDER_ID::Render_NonAlpha);
     SetMaterial("ShulkerTop_Mtrl", "Head", RENDER_ID::Render_Alpha);
+    Bones["Head"]->GetComponent<TransformComponent>()->SetParent(transform);
+
     Add_Bone("BottomHead", ObjectType::Bone, Bones["Body"], "ShulkerBottom_Mtrl");
     SetMaterial("ShulkerBottom_Mtrl", "BottomHead", RENDER_ID::Render_Alpha);
+    Bones["BottomHead"]->GetComponent<TransformComponent>()->SetParent(transform);
 
     SetScale(_vec3(10.f * Scale, 10.f * Scale, 10.f * Scale), "Head");
-    SetPosition(_vec3(0.f * Scale, 5.f * Scale, 0.f * Scale), "Head");
+    SetPosition(_vec3(0.f * Scale, -2.f * Scale, 0.f * Scale), "Head");
     //body
     SetScale(_vec3(5.f * Scale, 5.f * Scale, 5.f * Scale), "Body");
     SetScale(_vec3(10.f * Scale, 10.f * Scale, 10.f * Scale), "BottomHead");
@@ -97,24 +150,154 @@ void Shulker::InitTransform(ObjectType objType)
 
 void Shulker::InitTree()
 {
+    //blackboard
+    BlackBoard* bb = BlackBoard::Create();
+    bb->SetValue("Self", this);
+    bb->SetValue("Target", owner->GetObjectList(ObjectType::Player).back());
+    Distance = new float(60.f);
+    bb->SetValue("Distance", Distance);
+    IsAttack = new _bool(false);
+    bb->SetValue("IsAttack", IsAttack);
+
+    //BT
+    IsTargetInAttackRange* attackCheck = new IsTargetInAttackRange(new AttackNode());
+
+    SelectorNode* BehaviorNode = new SelectorNode();
+    BehaviorNode->AddChild(attackCheck);
+
+    IsAliveNode* IsAlive = new IsAliveNode(BehaviorNode);
+
+    SelectorNode* root = new SelectorNode();
+    root->AddChild(IsAlive);
+    root->AddChild(new DieNode());
+
+    BehaviorTree* bt = BehaviorTree::Create(root);
+
+    //AI
+    auto AI = AddComponent<AIController>(bt, bb);
 }
 
 void Shulker::InitAnimation()
 {
+    AttackAnim.ElapsedTime = 0.f;
+    AttackAnim.TotalTime = 0.5f;
+    AttackAnim.Phase = Ready;
+    AttackAnim.DelayTime = 0.f;
+
+    DieAnim.Start = 0;                  //start angle
+    DieAnim.End = 90.f;                //end angle
+    DieAnim.ElapsedTime = 0.f;
+    DieAnim.TotalTime = 0.7f;
 }
 
 void Shulker::PlayAnimation(_float dt)
 {
+    switch (State)
+    {
+    case MonsterState::Attack:
+        if (!AttackAnim.IsRunning) AttackAnim.IsRunning = true;
+        PlayAttack(dt);
+        break;
+    case MonsterState::Hit:
+        if (!HitAnim.IsRunning) HitAnim.IsRunning = true;
+        PlayHit(dt);
+        break;
+    case MonsterState::Die:
+        if (!DieAnim.IsRunning) DieAnim.IsRunning = true;
+        PlayDie(dt);
+        break;
+    }
 }
 
-void Shulker::PlayIdle(_float dt)
+void Shulker::PlayAttack(_float dt)
 {
+    switch (AttackAnim.Phase)
+    {
+    case Ready:
+    {
+        AttackAnim.DelayTime -= dt;
+        if (AttackAnim.DelayTime > 0.f) return;
+
+        AttackAnim.ElapsedTime += dt;
+
+        _float t = clamp(AttackAnim.ElapsedTime / AttackAnim.TotalTime, 0.f, 1.f);
+        _float pos = lerp(-2.f, 15.f, t);
+        Bones["Head"]->GetComponent<TransformComponent>()->SetPosition(_vec3(0.f * Scale, pos * Scale, 0.f * Scale));
+
+        if (AttackAnim.ElapsedTime > AttackAnim.TotalTime)
+        {
+            AttackAnim.Phase = Action;
+            AttackAnim.TotalTime = 0.5f;
+            AttackAnim.ElapsedTime = 0.f;
+
+            auto player = owner->GetFrontObject(ObjectType::Player);
+            _vec3 playerpos = player->GetComponent<TransformComponent>()->GetPosition();
+            _vec3 selfpos = GetComponent<TransformComponent>()->GetPosition();
+            _vec3 dir = playerpos - selfpos;
+            dir.y = 0;
+            Bones["Body"]->GetComponent<TransformComponent>()->SetForward(dir);
+        }
+        break;
+    }
+    case Action:
+    {
+        AttackAnim.ElapsedTime += dt;
+
+        if (AttackAnim.ElapsedTime > AttackAnim.TotalTime)
+        {
+            _vec3 pos = GetComponent<TransformComponent>()->GetPosition();
+            bullets[Index]->GetComponent<TransformComponent>()->SetPosition(pos);
+            bullets[Index++]->SetActive(true);
+            if (Index >= 10) Index = 0;
+            AttackAnim.Phase = Recover;
+            AttackAnim.TotalTime = 0.5f;
+            AttackAnim.ElapsedTime = 0.f;
+
+            EngineCore::GetInstance()->GetSoundManager()->PlaySFX("AttackShulker");
+        }
+        break;
+    }
+    case Recover:
+    {
+        AttackAnim.ElapsedTime += dt;
+
+        _float t = clamp(AttackAnim.ElapsedTime / AttackAnim.TotalTime, 0.f, 1.f);
+        _float pos = lerp(15.f, -2.f, t);
+        Bones["Head"]->GetComponent<TransformComponent>()->SetPosition(_vec3(0.f * Scale, pos * Scale, 0.f * Scale));
+
+        if (AttackAnim.ElapsedTime > AttackAnim.TotalTime)
+        {
+            AttackAnim.Phase = Ready;
+            AttackAnim.ElapsedTime = 0.f;
+            AttackAnim.TotalTime = 0.5f;
+            AttackAnim.DelayTime = 2.f;
+            *IsAttack = false;
+        }
+        break;
+    }
+    }
 }
 
 void Shulker::PlayDie(_float dt)
 {
+    DieAnim.ElapsedTime += dt;
+    
+    _float t = clamp(DieAnim.ElapsedTime / DieAnim.TotalTime, 0.f, 1.f);
+
+    _float Angle = lerp(DieAnim.Start, DieAnim.End, t);
+
+    GetComponent<TransformComponent>()->SetRotate({0.f, 0.f, -D3DXToRadian(Angle)});
+
+    if (DieAnim.ElapsedTime > DieAnim.TotalTime)
+    {
+        DieAnim.IsEnd = true;
+
+        SetDead();
+        DeleteBar();
+    }
 }
 
 void Shulker::Free()
 {
+    Monster::Free();
 }
---
+++ SR_Project/Client/Code/ShulkerBullet.cpp
@@ -0,0 +1,185 @@
+#include "pch.h"
+#include "ShulkerBullet.h"
+#include "TransformComponent.h"
+#include "MeshRendererComponent.h"
+#include "ObjectManager.h"
+#include "CollisionComponent.h"
+#include "EngineCore.h"
+#include "PhysicsComponent.h"
+#include "Bone.h"
+#include "InfoComponent.h"
+#include "Scene.h"
+#include "PhysicsSystem.h"
+
+ShulkerBullet::ShulkerBullet(ObjectManager* owner, ObjectType objType)
+	:Object(owner, objType)
+{
+}
+
+ShulkerBullet::~ShulkerBullet()
+{
+}
+
+ShulkerBullet* ShulkerBullet::Create(ObjectManager* owner, ObjectType objType)
+{
+    ShulkerBullet* Instance = new ShulkerBullet(owner, objType);
+
+    if (FAILED(Instance->Ready_Object(owner, objType)))
+    {
+        Safe_Release(Instance);
+        Instance = nullptr;
+    }
+    return Instance;
+}
+
+HRESULT ShulkerBullet::Ready_Object(ObjectManager* owner, ObjectType objType)
+{
+    auto transform = AddComponent<TransformComponent>();
+    auto collision = AddComponent<CollisionComponent>();
+    collision->AddCollider<AABBCollider>();
+    GetScene()->GetCollisionSystem()->RegisterCollision(collision);//test
+    collision->SetLayer(LAYER_PROJECTILE);
+    collision->SetMask(LAYER_PLAYER);
+    collision->SetCollisionEnter([this](Object* other) {this->OnCollisionEnter(other); });
+    collision->SetSize(_vec3(1.f, 1.f, 1.f));
+
+    auto physics = AddComponent<PhysicsComponent>();
+    GetScene()->GetPhysicsStstem()->RegisterBody(physics);
+    physics->SetKinematic(true);
+    physics->SetMass(1.f);
+
+    Bones.push_back(Bone::Create(owner, ObjectType::Bone, _vec3(0.4f, 0.4f, 0.1f), this, "ShulkerBullet_Mtrl"));
+    Bones.push_back(Bone::Create(owner, ObjectType::Bone, _vec3(0.4f, 0.4f, 0.1f), this, "ShulkerBullet_Mtrl"));
+    Bones.push_back(Bone::Create(owner, ObjectType::Bone, _vec3(0.4f, 0.4f, 0.1f), this, "ShulkerBullet_Mtrl"));
+    
+    Bones[0]->GetComponent<TransformComponent>()->SetRotate(0.f, D3DXToRadian(- 90.f), 0.f);
+    Bones[1]->GetComponent<TransformComponent>()->SetRotate(D3DXToRadian(-90.f), 0.f, 0.f);
+    Bones[2]->GetComponent<TransformComponent>()->SetRotate(0.f, 0.f, 0.f);
+    
+    for (auto& bone : Bones)
+        owner->AddObject(ObjectType::Bone, bone);
+
+    SetActive(true);
+    return S_OK;
+}
+
+void ShulkerBullet::Update(_float dt)
+{
+    Object::Update(dt);
+    if (Active)
+    {
+        ActiveTimer += dt;
+    
+        if (ActiveTimer > 15.f) SetActive(false);
+        
+        auto transform = GetComponent<TransformComponent>();
+        _vec3 rot = transform->GetRotate();
+        transform->SetRotate(rot.x + dt* 4.f, rot.y + dt * 8.f, rot.z + dt * 2.f);
+        
+    }
+    CheckDir();
+    MoveToDir(dt);
+}
+
+void ShulkerBullet::Late_Update(_float dt)
+{
+    Object::Late_Update(dt);
+}
+
+void ShulkerBullet::SetActive(bool active)
+{
+    Active = active;
+    if (Active)
+    {
+        for (auto& bone : Bones)
+        {
+            bone->GetComponent<MeshRenderer>()->SetRenderID(RENDER_ID::Render_NonAlpha);
+            GetComponent<CollisionComponent>()->SetSize(_vec3(1.f, 1.f, 1.f));
+
+            RotateCount = 0;
+            ActiveTimer = 0.f;
+        }
+    }
+    else
+    {
+        for (auto& bone : Bones)
+        {
+            bone->GetComponent<MeshRenderer>()->SetRenderID(RENDER_ID::Render_None);
+            GetComponent<CollisionComponent>()->SetSize(_vec3(0.f, 0.f, 0.f));
+
+            RotateCount = 0;
+            ActiveTimer = 0.f;
+        }
+    }
+}
+
+void ShulkerBullet::CheckDir()
+{
+    if (!Active) return;
+
+    auto player = owner->GetFrontObject(ObjectType::Player);
+    _vec3 playerpos = player->GetComponent<TransformComponent>()->GetPosition();
+
+    _vec3 selfpos = GetComponent<TransformComponent>()->GetPosition();
+
+    _vec3 dir = playerpos - selfpos;
+
+    if (abs(dir.x) < abs(dir.z))
+    {
+        if (dir.z > 0) CurDir = U;
+        else CurDir = D;
+    }
+    else
+    {
+        if (dir.x > 0) CurDir = R;
+        else CurDir = L;
+    }
+
+    if (PreDir != CurDir)
+    {
+        ++RotateCount;
+        PreDir = CurDir;
+    }
+}
+
+void ShulkerBullet::MoveToDir(_float dt)
+{
+    if (!Active) return;
+    //if (RotateCount >= 3) SetActive(false);
+
+    auto transform = GetComponent<TransformComponent>();
+    switch (CurDir)
+    {
+    case L:
+        transform->Translate(_vec3(-5.f * dt, 0.f, 0.f));
+        break;
+    case R:
+        transform->Translate(_vec3(5.f * dt, 0.f, 0.f));
+        break;
+    case U:
+        transform->Translate(_vec3(0.f, 0.f, 5.f * dt));
+        break;
+    case D:
+        transform->Translate(_vec3(0.f, 0.f, -5.f * dt));
+        break;
+    }
+}
+
+void ShulkerBullet::OnCollisionEnter(Object* other)
+{
+    if (!Active) return;        //!active -> nocollision
+
+    ObjectType objType = other->GetObjectType();
+    if (objType == ObjectType::Player)
+    {
+        auto playerStat = other->GetComponent<InfoComponent<PlayerInfo>>();
+        playerStat->AddHp(-20);
+
+        SetActive(false);
+    }
+}
+
+void ShulkerBullet::Free()
+{
+    Object::Free();
+}
