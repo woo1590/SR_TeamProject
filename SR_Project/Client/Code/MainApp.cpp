d6364890cca12111dd7268a7401650e0507eafbf
woo <leewooseok1114@gmail.com>
2025-07-22 17:21:39 +0900

    병합 후 수정

--- SR_Project/Client/Code/MainApp.cpp
+++ SR_Project/Client/Code/MainApp.cpp
@@ -59,7 +59,7 @@ HRESULT MainApp::Ready_MainApp(HINSTANCE hInst, int nCmdShow)
     EngineCore::GetInstance()->GetSoundManager()->PlayBGM("IntroBGM");
 
     /*---------Start Scene----------*/
-    Scene* Start = LoadingScene::Create(LOADID::Stage1);
+    Scene* Start = LoadingScene::Create(LOADID::Village);
     Core->GetSceneManager()->SetActiveScene(Start);
 
     isRunning = true;
